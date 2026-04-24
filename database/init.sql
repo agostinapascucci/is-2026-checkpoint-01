8730d139fe0f5cc34d9090a081417cc715bd3a23
Santiago Talavera <santiago.talavera@alu.frlp.utn.edu.ar>
2026-04-24 16:58:04 -0300

    fix(database): corrige error de sintaxis en rows de insercion de datos

--- database/init.sql
+++ database/init.sql
@@ -14,5 +14,5 @@ VALUES
     ('Agustina', 'Egüen', '33191', 'Frontend — Página Web con HTML y JavaScript', 'frontend', 'running'),
     ('Joaquin', 'Montes', '33459', 'Backend — API REST con Python y Flask', 'backend', 'running'),
     ('Santiago', 'Talavera', '33167', 'Base de Datos con PostgreSQL', 'database', 'running'),
-    ('Nicolas', 'Perez', '33177', 'Panel de Monitoreo con Portainer', 'portainer', 'running');
+    ('Nicolas', 'Perez', '33177', 'Panel de Monitoreo con Portainer', 'portainer', 'running'),
     ('Justina', 'Smith', '33346', 'Feature adicional', 'sin definir', 'running');
